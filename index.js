826545d62fbf4af3fee12b995082bef770a9beb7
monika.golebska <monika.golebska@vazco.eu>
2018-04-16 18:26:20 +0200

    update

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const puppeteer = require('puppeteer');
 async function run () {
   const browser = await puppeteer.launch({args: ['--no-sandbox'], headless: !!process.env.CI});
   const page = await browser.newPage();
-  await page.goto('https://www.google.com');
+  await test(browser, page);
   await browser.close();
+}
+
+async function test (browser, page) {
+  await page.goto('https://www.google.com');
 }
 
 run().catch(error => {
